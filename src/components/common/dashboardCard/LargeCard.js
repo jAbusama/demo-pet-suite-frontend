00a7cec21629968638d1f86da9b61d516405e913
Jep Abusama <jepoymabusama16@gmail.com>
2020-06-22 07:25:24 +0800

    hot fix

--- src/components/common/dashboardCard/LargeCard.js
+++ src/components/common/dashboardCard/LargeCard.js
@@ -2,34 +2,31 @@ import React from 'react';
 
 function LargeCard({ data }) {
   return (
-    <div className="card shadow mb-4">
-      <div className="card-header py-3">
-        <h6 className="m-0 font-weight-bold text-primary">Project</h6>
+    <div className='card shadow mb-4'>
+      <div className='card-header py-3'>
+        <h6 className='m-0 font-weight-bold text-primary'>Project</h6>
       </div>
-      <div className="card-body">
-        {
-          data.map(data => (
-            <div key={data.label}>
-               <h4 className="small font-weight-bold">
-                {data.name}
-                <span className="float-right">{data.label}</span>
-              </h4>
-              <div className="progress mb-4">
-                <div 
-                  className={`progress-bar ${data.bgColor}`}
-                  role="progressbar"
-                  style={{ width: `${data.value}%` }}
-                  aria-valuenow={`${data.value}`}
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                  >
-                </div>
-              </div>
+      <div className='card-body'>
+        {data.map((data) => (
+          <div key={data.label}>
+            <h4 className='small font-weight-bold'>
+              {data.name}
+              <span className='float-right'>{data.label}</span>
+            </h4>
+            <div className='progress mb-4'>
+              <div
+                className={`progress-bar ${data.bgColor}`}
+                role='progressbar'
+                style={{ width: `${data.value}%` }}
+                aria-valuenow={`${data.value}`}
+                aria-valuemin='0'
+                aria-valuemax='100'
+              ></div>
             </div>
-          ))
-        }
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 export default LargeCard;
--- src/public/pages/blog/index.js
+++ src/public/pages/blog/index.js
@@ -1,58 +1,63 @@
-import React from 'react'
-import PublicLayout from '../../../layouts/public'
-import Banner from '../../components/banner'
-import VideoSection from '../../components/videoSection'
-import{ eventsList, newsList, news } from '../../variables/blogList'
-import BlogCard from '../../components/blogCard'
-import bg_image_1 from '../../../images/big_image_1.jpg'
+import React from 'react';
+import PublicLayout from '../../../layouts/public';
+import Banner from '../../components/banner';
+import VideoSection from '../../components/videoSection';
+import { eventsList, newsList } from '../../variables/blogList';
+import BlogCard from '../../components/blogCard';
+import bg_image_1 from '../../../images/big_image_1.jpg';
 
-
-function Blog({history}) {
-  const heading = 'News and Events'
-  const introText = 'Read our daily news and events of our luxury suites.'
-  return(
-    <PublicLayout history={ history }>
-      
-      <Banner bg_media={ bg_image_1 } heading={ heading } introText= { introText } />
+function Blog({ history }) {
+  const heading = 'News and Events';
+  const introText = 'Read our daily news and events of our luxury suites.';
+  return (
+    <PublicLayout history={history}>
+      <Banner bg_media={bg_image_1} heading={heading} introText={introText} />
 
       <section className='padding' id='blog-news'>
-        <div className="container">
-          <div className="row ">
-            <div className="col-12 text-center">
-              <h2 className="heading">News</h2>
+        <div className='container'>
+          <div className='row '>
+            <div className='col-12 text-center'>
+              <h2 className='heading'>News</h2>
             </div>
 
-              {newsList.map(news => (
-                <div  key={news.key} className="col-sm-12 col-md-6 col-lg-4">
-                  <BlogCard media={ news.media } category={ news.category } title={ news.title } text={ news.text } />
-                </div>
-              ))}
-
+            {newsList.map((news) => (
+              <div key={news.key} className='col-sm-12 col-md-6 col-lg-4'>
+                <BlogCard
+                  media={news.media}
+                  category={news.category}
+                  title={news.title}
+                  text={news.text}
+                />
+              </div>
+            ))}
           </div>
         </div>
       </section>
 
       <section className='padding' id='blog-events'>
-        <div className="container">
-          <div className="row ">
-            <div className="col-12 text-center">
-              <h2 className="heading">Events</h2>
+        <div className='container'>
+          <div className='row '>
+            <div className='col-12 text-center'>
+              <h2 className='heading'>Events</h2>
             </div>
 
-              {eventsList.map(news => (
-                <div  key={news.key} className="col-sm-12 col-md-6 col-lg-4">
-                  <BlogCard media={ news.media } category={ news.category } title={ news.title } text={ news.text } />
-                </div>
-              ))}
-
+            {eventsList.map((news) => (
+              <div key={news.key} className='col-sm-12 col-md-6 col-lg-4'>
+                <BlogCard
+                  media={news.media}
+                  category={news.category}
+                  title={news.title}
+                  text={news.text}
+                />
+              </div>
+            ))}
           </div>
         </div>
       </section>
 
       <VideoSection />
-
     </PublicLayout>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
