6724a8d006ab3c5f2111d015ffdb2ad818f526ca
Jep Abusama <jepoymabusama16@gmail.com>
2021-02-14 21:58:16 +0800

    fix routing issue of react netlify

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import routeLinks from './Routes'
-import storage  from 'store'
-import useGlobal from './services/useGlobal'
-import './App.css'
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import routeLinks from './Routes';
+import storage from 'store';
+import useGlobal from './services/useGlobal';
+import './App.css';
 
 function App() {
-
   // storage.clearAll()
-  const [gStates, gActions] = useGlobal()
-  let isLogin = storage.get('isLogin')
-  let user = storage.get('user')
-  
+  const [gStates, gActions] = useGlobal();
+  let isLogin = storage.get('isLogin');
+  let user = storage.get('user');
+
   useEffect(() => {
-    if(isLogin) {
-      gActions.getLogin(user.id)
+    if (isLogin) {
+      gActions.getLogin(user.id);
     }
-  },[])
-  
+  }, []);
+
   return (
-      <Router>
-        <div className='App'>
-          <Switch>
-            { routeLinks.map(route => <Route {...route} />) }
-          </Switch>
-        </div>
-      </Router>
-  )
+    <Router>
+      <div className='App'>
+        <Switch>
+          {routeLinks.map((route) => (
+            <Route {...route} />
+          ))}
+          <Route path='*' component={() => <div>404 Page</div>} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
